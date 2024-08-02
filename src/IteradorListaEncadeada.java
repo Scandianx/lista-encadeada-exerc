7a08c61146a3456a3fc02da80214e6c30acb8509
Scandianx <lipescandiani@gmail.com>
2024-08-01 21:03:10 -0300

    commit inicial

---
+++ src/IteradorListaEncadeada.java
@@ -0,0 +1,23 @@
+
+
+public class IteradorListaEncadeada implements Iterador{
+
+	No noAtual; 
+	
+	public IteradorListaEncadeada(No no) {
+		this.noAtual = no;
+	}
+	
+	public boolean temProximo() {
+		if (this.noAtual != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int obterProximoElemento() {
+		No auxiliar = this.noAtual;
+		this.noAtual = this.noAtual.obterProximo();
+		return auxiliar.obterDado();
+	}
+}
---
+++ src/TesteListaManipulacaoFim.java
@@ -0,0 +1,40 @@
+
+
+public class TesteListaManipulacaoFim {
+
+	public static void main(String[] args) {
+		ListaEncadeadaSimples listaEncadeada = new ListaEncadeadaSimples();
+		
+		System.out.println("\n------------ Após a inclusão ------------");
+		listaEncadeada.adicionarFim(10);
+		listaEncadeada.adicionarFim(20);
+		listaEncadeada.adicionarFim(30);
+		listaEncadeada.adicionarFim(40);
+		listaEncadeada.adicionarFim(50);
+		listaEncadeada.adicionarFim(60);
+		listaEncadeada.adicionarFim(80);
+		listaEncadeada.incluirEmOrdemCrescente(0);
+		listaEncadeada.excluirDadoEspecifico(30);
+		System.out.println(listaEncadeada); //O mesmo que: System.out.println(listaEncadeada.toString());
+			
+		System.out.println("\n\n------------ Após uma exclusão ------------");	
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada);
+		
+		System.out.println("\n------------ Após mais duas exclusões ------------");	
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada);
+		
+		System.out.println("\n------------ Após mais quatro exclusões ------------");	
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada);
+		
+		System.out.println("\n------------ Após mais uma exclusão ------------");	
+		System.out.println(listaEncadeada.excluirFim());
+		System.out.println(listaEncadeada);
+	}
+}
